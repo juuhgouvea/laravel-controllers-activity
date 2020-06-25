bcff2e99964aff04802116655a854916729c8dfb
juuhgouvea <juliagouveasantos@gmail.com>
2020-06-25 00:42:59 -0300

    redireciona rota principal

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('default');
-});
-
-// Route::redirect('/', '/cidades', 301);
+Route::redirect('/', '/cidades', 301);
 Route::resource('cidades', 'CidadeController');
